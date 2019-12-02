dc4d872a2a0476a353b03fcd723b085436972c4f
learn <learn@LEARN-16.local>
2019-12-02 11:48:48 -0800

    react working

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1> Suntracker App </h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
