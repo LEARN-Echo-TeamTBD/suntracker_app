29f2c54e45c7068ac30b847caf31f8d09e2ee03c
learn <learn@LEARN-16.local>
2019-12-02 11:59:06 -0800

    Jest and Rspec working with App.js

---
+++ app/javascript/tests/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('Renders a welcome title', ()=>{
+    const app = mount(<App />)
+    expect(app.find('h1').text()).toEqual(' Suntracker App ')
+})
