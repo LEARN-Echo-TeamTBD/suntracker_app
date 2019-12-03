8878e9f029fba9b2e3cd420b617500d270ca3c1b
learn <learn@LEARN-16.local>
2019-12-03 13:42:59 -0800

    error change in Header

--- app/javascript/reactcomponents/Header.js
+++ app/javascript/reactcomponents/Header.js
@@ -19,7 +19,7 @@ class Header extends React.Component {
 
             <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active" key="1">
+              <li className="nav-item active">
                 <NavLink className="nav-link" tag={Link} to="/userform">Your Logger <span className="sr-only">(current)</span></NavLink>
               </li>
               <li className="nav-item">
