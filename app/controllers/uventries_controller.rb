bd9f6b0db07dca8e4bf2d26ea9d04658e817c874
learn <learn@LEARN-16.local>
2019-12-05 11:34:18 -0800

    db:migrate both user update | uventry mirgation

---
+++ app/controllers/uventries_controller.rb
@@ -0,0 +1,2 @@
+class UventriesController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_212139) do
+ActiveRecord::Schema.define(version: 2019_12_05_192222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,22 @@ ActiveRecord::Schema.define(version: 2019_12_02_212139) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
+    t.integer "skintone"
+    t.boolean "skin_cancer_history"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "uventries", force: :cascade do |t|
+    t.integer "hours_in_sun"
+    t.boolean "sun_block_application"
+    t.integer "lattitude"
+    t.integer "longitude"
+    t.string "time_of_day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
