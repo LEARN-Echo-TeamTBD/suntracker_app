2bebc2863c6b363c59bfbdef082facdf06de6a25
learn <learn@LEARN-16.local>
2019-12-06 16:52:07 -0800

    styled the uv entry form

--- app/javascript/pages/UserForm.js
+++ app/javascript/pages/UserForm.js
@@ -5,6 +5,7 @@ class UserForm extends React.Component {
   constructor(props){
       super(props)
       this.state = {
+          createSuccess:false,
           sun_block_application: false,
           form: {
               hours_in_sun: 0,
@@ -16,36 +17,55 @@ class UserForm extends React.Component {
       }
   }
 
-  handleButton = (event) => {
+  handleClick = (event) => {
       this.setState({sun_block_application: event.target.value})
+      // need to fix the button to flip color
   }
 
-  handleChange = () => {
-      let {form} = this.state
+  handleChange = () => {sun_block_application
+      const {form} = this.state
       form[event.target.name] = event.target.value
       this.setState({form: form})
   }
 
   handleSubmit = () => {
-      // submit to the handleNewEntry from parent
+      // submit to the handleNewEntry from parent and clear the input form;
+      // need to implement handleNewEntry in parent
+
+      // const { form } = this.state
+      // this.props.handleNewEntry(form)
+      // .then( () => {
+      //     this.setState({
+      //         createSuccess:false,
+      //         sun_block_application: false,
+      //         form: {
+      //             hours_in_sun: 0,
+      //             lattitude: 0,
+      //             longitude: 0,
+      //             time_of_day: '',
+      //             additional_information: ''
+      //         }
+      //     })
+      // })
   }
 
   render () {
+      console.log(this.state.sun_block_application);
     return (
         <React.Fragment>
         <h1>Traq yo page</h1>
         <div className="form-border-center">
             <h4>Did you apply sun protection today?</h4>
-            <button name='sun_block_application' value={true} type="button" className="btn-margin" className="btn btn-outline-info" onClick={this.handleButton}>Yes</button>
-            <button value={false} type="button" className=" btn-margin" className="btn btn-outline-info" onClick={this.handleButton}>No</button>
+            <button name='sun_block_application' value={true} type="button"
+            className={this.state.sun_block_application == true ? "btn btn-info" : "btn btn-outline-info"} onClick={this.handleClick}>Yes</button>
+            <button value={false} type="button" className={this.state.sun_block_application == false ? "btn btn-warning" : "btn btn-outline-warning" }  onClick={this.handleClick}>No</button>
 
             <fieldset className="form-group">
-              <legend>Hours of sun exposure</label>
-              <p> (need to fix the default slider position and change it to all the way left or middle) </p>
-              </legend>
-              <label for="customRange1">Example range</p>
-              <input name='hours_in_sun' type="range" className="custom-range" id="customRange1" min='0' max='48' onChange={this.handleChange}/>
-              <output for="customRange1" >Your sun exposure: {this.state.form.hours_in_sun/2}</output>
+              <legend>Hours of sun exposure</legend>
+                  <p> (need to fix the default slider position and change it to all the way left or middle) </p>
+              <label htmlFor="customRange1">Example range</label>
+              <input name='hours_in_sun' type="range" className="custom-range" id="customRange1" min="0" max="48" value={this.state.form.hours_in_sun} onChange={this.handleChange}/>
+              <output htmlFor="customRange1" >Your sun exposure: {this.state.form.hours_in_sun/2}</output>
             </fieldset>
 
             <h4>Additional information</h4>
