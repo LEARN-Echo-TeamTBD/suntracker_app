aaad1e5139acd41b3958afecd9f7f92d2a57e6b6
Jortega23 <jortegs23@gmail.com>
2019-12-09 15:05:30 -0800

    homepage changes

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -9,7 +9,22 @@ class Home extends React.Component {
     return (
         <React.Fragment>
             <div className="homePage">
-                <h1>Welcome to Suntracker</h1>
+              <div className="hometitle">
+                  <h1>SunTracker</h1>
+              </div>
+              <div className='mission'>
+                    <p>Our mission help promote sun awarness with a simple, fun and interactive system. We encourgae our users to log daily to help track the amount of sun exposure they are recieving on a day to day basis.
+                      We display our users there data to help them visually keep track of there exposure, along with providing rescources to help care for there skin.
+                       </p>
+                  </div>
+                  <hr></hr>
+                  <div className='about'>
+                    <h2>About SunTracker</h2>
+                      <p> We have designed a simple tracker system that allows our users to log in daily sun exposure data helping them stay on top of there sun exposure.</p>
+                          <li>Track and log daily sun exposure</li>
+                          <li>Recieve daily sun analytics</li>
+                          <li>Generate graph displaying user sun data</li>
+                  </div>
             </div>
         </React.Fragment>
     );
--- app/javascript/pages/Resources.js
+++ app/javascript/pages/Resources.js
@@ -1,13 +1,61 @@
 import React from "react"
 
+
 class Resources extends React.Component {
 
   render () {
     return (
         <React.Fragment>
-            <div>
-                <h1>Resource x page</h1>
+            <div className="resources">
+                <h1>Resources</h1>   
             </div>
+
+
+
+          <div className="allCards">
+            <div className="cardOne">
+              <div className="card border-primary mb-3">
+                 <div className="card-header">Resource</div>
+                  <div className="card-body">
+                  <h4 className="card-title"><a href="https://www.skincancer.org/early-detection/self-exams/" target="_blank">Self Exams:</a></h4>
+                  <p className="card-text">Self exams are a good way of keeping track of any changes in moles or any new spot that may have come up.
+                    This is a simple and effective way to stay on top of your skin.</p>
+                </div>
+             </div>
+            </div>
+            
+            <div className="cardTwo">
+             <div className="card border-primary mb-3">
+                 <div className="card-header">Resource</div>
+                  <div className="card-body">
+                  <h4 className="card-title"><a href="https://www.medicalnewstoday.com/articles/320639.php#how-to-protect-each-skin-type" target="_blank">Fitzpatrick Scale:</a></h4>
+                  <p className="card-text">The Fitzpatrick scale is a great resource to know what your skin type better when it comes to sun exposure.</p>
+                </div>
+             </div>
+            </div>
+          
+            <div className="cardThree">
+             <div className="card border-primary mb-3">
+                 <div className="card-header">Resource</div>
+                  <div className="card-body">
+                  <h4 className="card-title"><a href="https://www.preventcancer.org/programs/save-your-skin/" target="_blank">ABCD of a Mole</a></h4>
+                  <p className="card-text">Asymmetry, Border, Color, Diameter of a mole.</p>
+                </div>
+             </div>
+            </div>
+
+            <div className="cardFour">
+             <div className="card border-primary mb-3">
+                 <div className="card-header">Resource</div>
+                  <div className="card-body">
+                  <h4 className="card-title"><a href="https://www.aad.org/diseases/skin-cancer/what-is-mohs-surgery" target="_blank">Mohs Surgery</a></h4>
+                  <p className="card-text">A very effective provedure to cure skin cancer. Mohs Surgery.</p>
+                </div>
+             </div>
+            </div>
+          </div>
+
+
         </React.Fragment>
     );
   }
