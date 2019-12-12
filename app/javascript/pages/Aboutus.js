ad63c8d661d69103f52eab873e90159fb7c4ada5
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-12-12 12:17:48 -0800

    update team cards

--- app/javascript/pages/Aboutus.js
+++ app/javascript/pages/Aboutus.js
@@ -5,32 +5,62 @@ import React from "react"
 import Github from './github2.png'
 
 class Aboutus extends React.Component {
- 
+
 
   render () {
     return (
         <React.Fragment>
-               <div className = "member-cards-container">
-                 <div className = "member-card">
-                  <div className="card mb-3">
-                  <h3 className="card-header">
-                  </h3>
-                  <div className="card-body">
-                    <h5 className="card-title" className='member-age'>22</h5>
+          <div class="container">
+                <div class="row pb-5">
+                  <div class="col-xl-3 col-sm-6">
+        						<div class="team-card-style-1">
+        						<div class="team-card-inner">
+                        <div class="team-thumb"><img src="https://demo.createx.studio/createx/img/team/style-1/01.jpg" alt="Team Member Picture" />
+                        </div>
+        								<h4 class="team-name">William Smith</h4>
+        								<span class="team-position">Lead Programmer</span>
+        								<a class="team-contact-link" href="tel:+19871625346"><i class="fe-icon-phone"></i>&nbsp;+1 (987) 162 53 46</a><a class="team-contact-link" href="mailto:info@example.com"><i class="fe-icon-mail"></i>&nbsp;info@example.com</a>
+                        <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-twitter" href="github.com"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" height="42" width="42" /></a><a class="social-btn sb-style-4 sb-github" href="#"><i class="socicon-github"></i></a><a class="social-btn sb-style-4 sb-stackoverflow" href="#"><i class="socicon-stackoverflow"></i></a><a class="social-btn sb-style-4 sb-skype" href="#"><i class="socicon-skype"></i></a></div>
+
+                      </div>
+                    </div>
                   </div>
-                  <img style={{height: "auto", width: "100%", display: "block"}} src="https://live.staticflickr.com/68/172629460_f55d8b084d_z.jpg" alt="Card image" />
-                  <div className="card-body">
-                  <p className="card-text" className='member-enjoys'>name</p>
+
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="team-card-style-1 mx-auto">
+                      <div class="team-card-inner">
+                        <div class="team-thumb"><img src="https://demo.createx.studio/createx/img/team/style-1/02.jpg" alt="Team Member Picture" />
+                        </div>
+                        <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-facebook" href="#"><i class="socicon-facebook"></i></a><a class="social-btn sb-style-4 sb-twitter" href="#"><i class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-google-plus" href="#"><i class="socicon-googleplus"></i></a><a class="social-btn sb-style-4 sb-linkedin" href="#"><i class="socicon-linkedin"></i></a></div><a class="team-contact-link" href="tel:+57872336590"><i class="fe-icon-phone"></i>&nbsp;+5 (787) 233 65 90</a><a class="team-contact-link" href="mailto:info@example.com"><i class="fe-icon-mail"></i>&nbsp;info@example.com</a>
+                      </div><span class="team-position">Marketing Manager</span>
+                      <h4 class="team-name">Samantha Palson</h4>
+                    </div>
                   </div>
-                  <div className="card-body">
-                     <a href="https://github.com/"  target="_blank" class="card-link"><i class="fab fa-github"></i> github</a>
-                     <a href="#" class="card-link">Delete</a>
-                   </div>
-                   <div className="card-footer text-muted">
-                   </div>
+
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="team-card-style-1 mx-auto">
+                      <div class="team-card-inner">
+                        <div class="team-thumb"><img src="https://demo.createx.studio/createx/img/team/style-1/03.jpg" alt="Team Member Picture" />
+                        </div>
+                        <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-twitter" href="#"><i class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-pinterest" href="#"><i class="socicon-pinterest"></i></a><a class="social-btn sb-style-4 sb-behance" href="#"><i class="socicon-behance"></i></a><a class="social-btn sb-style-4 sb-dribbble" href="#"><i class="socicon-dribbble"></i></a></div><a class="team-contact-link" href="tel:+13907765843"><i class="fe-icon-phone"></i>&nbsp;+1 (390) 776 58 43</a><a class="team-contact-link" href="mailto:info@example.com"><i class="fe-icon-mail"></i>&nbsp;info@example.com</a>
+                      </div><span class="team-position">Graphic Designer</span>
+                      <h4 class="team-name">Emma Johnson</h4>
+                    </div>
+                  </div>
+
+                  <div class="col-xl-3 col-sm-6">
+                    <div class="team-card-style-1 mx-auto">
+                      <div class="team-card-inner">
+                        <div class="team-thumb"><img src="https://demo.createx.studio/createx/img/team/style-1/04.jpg" alt="Team Member Picture" />
+                        </div>
+                        <div class="team-social-bar text-center"><a class="social-btn sb-style-4 sb-facebook" href="#"><i class="socicon-facebook"></i></a><a class="social-btn sb-style-4 sb-twitter" href="#"><i class="socicon-twitter"></i></a><a class="social-btn sb-style-4 sb-skype" href="#"><i class="socicon-skype"></i></a><a class="social-btn sb-style-4 sb-linkedin" href="#"><i class="socicon-linkedin"></i></a></div><a class="team-contact-link" href="tel:+72056557984"><i class="fe-icon-phone"></i>&nbsp;+7 (205) 655 79 84</a><a class="team-contact-link" href="mailto:info@example.com"><i class="fe-icon-mail"></i>&nbsp;info@example.com</a>
+                      </div><span class="team-position">CEO, Co-Founder</span>
+                      <h4 class="team-name">James McCorvinus</h4>
+                    </div>
+                  </div>
                 </div>
-                </div>
-        </div>
+
+              </div>
         </React.Fragment>
     );
   }
