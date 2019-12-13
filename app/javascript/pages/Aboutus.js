2ead1e70a459558a08a4e3111869adb4dd845b2c
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-12-13 14:29:18 -0800

    update gradient color & add product description

--- app/javascript/pages/Aboutus.js
+++ app/javascript/pages/Aboutus.js
@@ -52,16 +52,6 @@ class Aboutus extends React.Component {
                 <li>Recieve daily sun analytics</li>
                 <li>Generate graph displaying user sun data</li>
               </ul>
-              <section class="module">
-                <h1 class="module__heading module__heading--b">Boxed</h1>
-                <ol class="custom-bullet custom-bullet--b">
-                  <li>Lorem ipsum dolor sit amet</li>
-                  <li>Consectetur adipiscing elit</li>
-                  <li>Integer molestie lorem at massa</li>
-                  <li>Facilisis in pretium nisl aliquet</li>
-                  <li>Nulla volutpat aliquam velit</li>
-                </ol>
-              </section>
               <hr className="my-4" />
               <div className="text-con">
                 <span className="underline-right">Our Team</span>
