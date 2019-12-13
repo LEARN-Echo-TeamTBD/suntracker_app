b1d08e344f9be9b11aebcea3661825c5099690b2
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-12-13 08:27:18 -0800

    fix team member card layout and add min width

--- app/javascript/pages/Aboutus.js
+++ app/javascript/pages/Aboutus.js
@@ -8,7 +8,7 @@ import StephanPhoto from '../../assets/images/stephan.jpeg'
 class Aboutus extends React.Component {
   state = {
    team:[
-      { 
+      {
         name:'Joyce He',
         image:JoycePhoto,
         logo: GithubLogo,
@@ -38,46 +38,43 @@ class Aboutus extends React.Component {
     }
     ]
   }
-  
-
 
   render () {
     return (
         <React.Fragment>
           <div className="container">
             <div className="to-center">
-              <h2>Our product</h2>
-              <hr class="my-4" />
-              <div class="text-con">
-                <span class="underline-right">Our Team</span>
+              <h2>Why SunTracker</h2>
+              <hr className="my-4" />
+              <div className="text-con">
+                <span className="underline-right">Our Team</span>
               </div>
              </div>
-              </div>
-            {this.state.team.map(member=>(
-              <div className='memberCard'>
-                <div className="row pb-5">
-                  <div className="col-xl-3 col-sm-6">
-        						<div className="team-card-style-1">
-        						<div className="team-card-inner">
-                        <div className="team-thumb">
-                          <img src={member.image} alt="Team Member Picture" />
-                        </div>
-        								<h4 className="team-name">{member.name}</h4>
-        								<span className="team-position">Programmer</span>
-        								<a className="team-contact-link" href="mailto:info@example.com"><i className="fe-icon-mail"></i>&nbsp;info@example.com</a>
-                        <div className="team-social-bar text-center">
-                          <a className="social-btn sb-style-4 sb-github" href={member.link}>
-                            <div className="social-icon">
-                            <img src={member.logo} />
-                            </div>
-                          </a>
-                          <a className="social-btn sb-style-4 sb-github" href="#"><i className="socicon-github"></i></a><a className="social-btn sb-style-4 sb-stackoverflow" href="#"><i className="socicon-stackoverflow"></i></a><a className="social-btn sb-style-4 sb-skype" href="#"><i className="socicon-skype"></i></a></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
+             <div class="row pb-5">
+               {this.state.team.map(member=>(
+                     <div className="col-xl-3 col-sm-6">
+                       <div className="team-card-style-1">
+                       <div className="team-card-inner">
+                           <div className="team-thumb">
+                             <img src={member.image} alt="Team Member Picture" />
+                           </div>
+                           <h4 className="team-name">{member.name}</h4>
+                           <span className="team-position">Programmer</span>
+                           <a className="team-contact-link" href="mailto:info@example.com"><i className="fe-icon-mail"></i>&nbsp;info@example.com</a>
+                           <div className="team-social-bar text-center">
+                             <a className="social-btn sb-style-4 sb-github" href={member.link}>
+                               <div className="social-icon">
+                               <img src={member.logo} />
+                               </div>
+                             </a>
+                             <a className="social-btn sb-style-4 sb-github" href="#"><i className="socicon-github"></i></a><a className="social-btn sb-style-4 sb-stackoverflow" href="#"><i className="socicon-stackoverflow"></i></a><a className="social-btn sb-style-4 sb-skype" href="#"><i className="socicon-skype"></i></a></div>
+                         </div>
+                       </div>
+                     </div>
+               ))}
+             </div>
+          </div>
+
         </React.Fragment>
     );
   }
