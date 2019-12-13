b4e69e827a053b97077f9cf635ca2d38a156cf35
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-12-13 08:54:43 -0800

    add mx-auto to make card center and responsive

--- app/javascript/pages/Aboutus.js
+++ app/javascript/pages/Aboutus.js
@@ -49,30 +49,30 @@ class Aboutus extends React.Component {
               <div className="text-con">
                 <span className="underline-right">Our Team</span>
               </div>
-             </div>
-             <div class="row pb-5">
-               {this.state.team.map(member=>(
-                     <div className="col-xl-3 col-sm-6">
-                       <div className="team-card-style-1">
-                       <div className="team-card-inner">
-                           <div className="team-thumb">
-                             <img src={member.image} alt="Team Member Picture" />
+               <div class="row pb-5">
+                 {this.state.team.map(member=>(
+                       <div className="col-xl-3 col-sm-6">
+                         <div className="team-card-style-1 mx-auto">
+                         <div className="team-card-inner">
+                             <div className="team-thumb">
+                               <img src={member.image} alt="Team Member Picture" />
+                             </div>
+                             <h4 className="team-name">{member.name}</h4>
+                             <span className="team-position">Programmer</span>
+                             <a className="team-contact-link" href="mailto:info@example.com"><i className="fe-icon-mail"></i>&nbsp;info@example.com</a>
+                             <div className="team-social-bar text-center">
+                               <a className="social-btn sb-style-4 sb-github" href={member.link}>
+                                 <div className="social-icon">
+                                 <img src={member.logo} />
+                                 </div>
+                               </a>
+                               <a className="social-btn sb-style-4 sb-github" href="#"><i className="socicon-github"></i></a><a className="social-btn sb-style-4 sb-stackoverflow" href="#"><i className="socicon-stackoverflow"></i></a><a className="social-btn sb-style-4 sb-skype" href="#"><i className="socicon-skype"></i></a></div>
                            </div>
-                           <h4 className="team-name">{member.name}</h4>
-                           <span className="team-position">Programmer</span>
-                           <a className="team-contact-link" href="mailto:info@example.com"><i className="fe-icon-mail"></i>&nbsp;info@example.com</a>
-                           <div className="team-social-bar text-center">
-                             <a className="social-btn sb-style-4 sb-github" href={member.link}>
-                               <div className="social-icon">
-                               <img src={member.logo} />
-                               </div>
-                             </a>
-                             <a className="social-btn sb-style-4 sb-github" href="#"><i className="socicon-github"></i></a><a className="social-btn sb-style-4 sb-stackoverflow" href="#"><i className="socicon-stackoverflow"></i></a><a className="social-btn sb-style-4 sb-skype" href="#"><i className="socicon-skype"></i></a></div>
                          </div>
                        </div>
-                     </div>
-               ))}
-             </div>
+                     ))}
+                 </div>
+              </div>
           </div>
 
         </React.Fragment>
