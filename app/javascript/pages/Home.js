b52d122696169459d59084ddea6c7090b7c06b31
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-12-16 15:55:08 -0800

    delete comment on home page and fix p tag indentation

--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -18,8 +18,7 @@ class Home extends React.Component {
             <h1 className="hometitle">SunTracker</h1>
             <img className="home-logo" src={Sunlogo} alt="logo"/>
                <div className="front-info">
-                    <p className='mission'>Our mission is help promote sun awarness with a simple, fun and interactive system. We encourage our users to track the amount of sun exposure they are recieving on a day to day basis.
- We display our users their data to help them visually keep track of there exposure, along with providing rescources to help care for there skin.
+                    <p className='mission'>Our mission is help promote sun awarness with a simple, fun and interactive system. We encourage our users to track the amount of sun exposure they are recieving on a day to day basis.We display our users their data to help them visually keep track of there exposure, along with providing rescources to help care for there skin.
                     </p>
                     <div className="home-buttons">
                       <a className="btn btn-primary" id="left-button" href={sign_in_route}>Sign in</a>
@@ -33,9 +32,4 @@ class Home extends React.Component {
     );
   }
 }
-//    <p className='mission'>Our mission is help promote sun awarness with a simple, fun and interactive system. We encourgae our users to log daily to help track the amount of sun exposure they are recieving on a day to day basis.
-// We display our users there data to help them visually keep track of there exposure, along with providing rescources to help care for there skin.
-// </p>
-// <LinkButton href="/" title="Sign In"/>
-// <LinkButton href="/" title="Read More"/>
 export default Home
