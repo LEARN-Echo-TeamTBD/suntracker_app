971aad9a1780fd3652693b8778b4030e039c0f29
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-12-17 15:33:07 -0800

    organize information to be shown in the feedback component

--- app/javascript/reactcomponents/Feedback.js
+++ app/javascript/reactcomponents/Feedback.js
@@ -5,7 +5,7 @@ const Feedback = (props) => {
   const current_input = props.data
   const {safe_exposure_time, uv, uv_max, hours_in_sun, sun_block_application} = current_input
 
-  const sunblockMessage = sun_block_application ? "Great job applying sunblock and make sure to re-apply if you plan to stay in the sun!" : "Protect your skin and remember to apply sunblock!"
+  const sunblockMessage = sun_block_application ? "✅Great job applying sunblock and make sure to re-apply if you plan to stay in the sun!" : "Protect your skin against the sun by applying sunblock if you plan to stay outside!"
 
   const currentUvInfo = (uv) => {
     if (uv <= 2) {
@@ -18,24 +18,24 @@ const Feedback = (props) => {
   }
 
     return (
-    <React.Fragment>
+    <div className="feedback">
         <h1 className="display-3">Welcome Back!</h1>
         <p className="lead">
           The current UV level is <span className={currentUvInfo(uv).className}>{uv.toFixed(2)}</span>.
         </p>
         <p class="text-muted">{currentUvInfo(uv).currentUvMessage}</p>
+        <p className={sun_block_application ? "text-success" : "text-danger"}> {sunblockMessage}</p>
         <p className="lead">The maximum UV level for today is <span className={currentUvInfo(uv_max).className}>{uv_max.toFixed(2)}</span>.
         </p>
         <p className="lead">
           Based on your skin tone, your <span className="badge badge-secondary">{hours_in_sun/2}</span> {hours_in_sun/2 <= 1 ? 'hour' : 'hours'} of  sun exposure {hours_in_sun/2 > (safe_exposure_time/60) ? <span className="badge badge-danger">exceeded</span> : <span className="badge badge-success">'is less than'</span>} the healthy amount of <span className="badge badge-secondary">{(safe_exposure_time/60).toFixed(1)}</span> hours.
         </p>
-        <p className={sun_block_application ? "text-success" : "text-danger"}> {sunblockMessage}</p>
         <Emoji
         skintone={props.user_skintone}
         current_sun_exposure={hours_in_sun/2}
         max_sun_exposure={safe_exposure_time}
         />
-    </React.Fragment>
+    </div>
     )
 }
 
