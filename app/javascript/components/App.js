d4b3f968feadd62a5bf4927aad774908bf18f647
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-02-23 20:16:22 -0800

    change about me styling

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -24,68 +24,86 @@ class App extends React.Component {
             <React.Fragment>
                 <Router>
                     <Header {...this.props} />
-                    <Switch>
-                        {logged_in && (
-                            <>
-                                <Route
-                                    path='/'
-                                    exact
-                                    render={props => (
-                                        <Home sign_in_route={sign_in_route} />
-                                    )}
-                                />
-                                <Route
-                                    path='/dashboard'
-                                    render={props => (
-                                        <Dashboard
-                                            user_id={this.props.current_user_id}
-                                            user_skintone={
-                                                this.props.current_user_skintone
-                                            }
-                                            user_cancer_history={
-                                                this.props
-                                                    .current_user_cancer_history
-                                            }
-                                        />
-                                    )}
-                                />
-                                <Route
-                                    path='/userform'
-                                    render={props => (
-                                        <UserForm
-                                            user_id={this.props.current_user_id}
-                                            user_skintone={
-                                                this.props.current_user_skintone
-                                            }
-                                        />
-                                    )}
-                                />
-                                <Route
-                                    path='/resources'
-                                    component={Resources}
-                                />
-                                <Route path='/aboutus' component={Aboutus} />
-                            </>
-                        )}
+                    <div id='container'>
+                        <Switch>
+                            {logged_in && (
+                                <>
+                                    <Route
+                                        path='/'
+                                        exact
+                                        render={props => (
+                                            <Home
+                                                sign_in_route={sign_in_route}
+                                            />
+                                        )}
+                                    />
+                                    <Route
+                                        path='/dashboard'
+                                        render={props => (
+                                            <Dashboard
+                                                user_id={
+                                                    this.props.current_user_id
+                                                }
+                                                user_skintone={
+                                                    this.props
+                                                        .current_user_skintone
+                                                }
+                                                user_cancer_history={
+                                                    this.props
+                                                        .current_user_cancer_history
+                                                }
+                                            />
+                                        )}
+                                    />
+                                    <Route
+                                        path='/userform'
+                                        render={props => (
+                                            <UserForm
+                                                user_id={
+                                                    this.props.current_user_id
+                                                }
+                                                user_skintone={
+                                                    this.props
+                                                        .current_user_skintone
+                                                }
+                                            />
+                                        )}
+                                    />
+                                    <Route
+                                        path='/resources'
+                                        component={Resources}
+                                    />
+                                    <Route
+                                        path='/aboutus'
+                                        component={Aboutus}
+                                    />
+                                </>
+                            )}
 
-                        {!logged_in && (
-                            <>
-                                <Route
-                                    path='/'
-                                    exact
-                                    render={props => (
-                                        <Home sign_in_route={sign_in_route} />
-                                    )}
-                                />
-                                <Route path='/signup' component={Signup} />
-                                <Route
-                                    path='/resources'
-                                    component={Resources}
-                                />
-                                <Route path='/aboutus' component={Aboutus} />
-                            </>
-                        )}
-                    </Switch>
+                            {!logged_in && (
+                                <>
+                                    <Route
+                                        path='/'
+                                        exact
+                                        render={props => (
+                                            <Home
+                                                sign_in_route={sign_in_route}
+                                            />
+                                        )}
+                                    />
+                                    <Route path='/signup' component={Signup} />
+                                    <Route
+                                        path='/resources'
+                                        component={Resources}
+                                    />
+                                    <Route
+                                        path='/aboutus'
+                                        component={Aboutus}
+                                    />
+                                </>
+                            )}
+                        </Switch>
+                    </div>
                     <Footer />
                 </Router>
             </React.Fragment>
--- app/javascript/pages/Home.js
+++ app/javascript/pages/Home.js
@@ -1,30 +1,48 @@
-import React from "react"
+/** @format */
+
+import React from 'react'
 import Sunlogo from '../../assets/images/sunlogo2.png'
 import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
-
-  render () {
-    const { sign_in_route } = this.props
-    return (
-          <React.Fragment>
-            <div className="homePage">
-            <h1 className="hometitle">SunTracker</h1>
-            <img className="home-logo" src={Sunlogo} alt="logo"/>
-               <div className="front-info">
-                    <p className='mission'>Our mission is help promote sun awarness with a simple, fun and interactive system. We encourage our users to track the amount of sun exposure they are recieving on a day to day basis.We display our users their data to help them visually keep track of there exposure, along with providing rescources to help care for there skin.
-                    </p>
-                    <div className="home-buttons">
-                      <a className="btn btn-primary" id="left-button" href={sign_in_route}>Sign in</a>
-                      <Link to="/aboutus" className="btn btn-outline-primary" id="right-button">
-                        About us
-                      </Link>
+    render() {
+        const { sign_in_route } = this.props
+        return (
+            <React.Fragment>
+                <div className='homePage'>
+                    <h1 className='hometitle'>SunTracker</h1>
+                    <img className='home-logo' src={Sunlogo} alt='logo' />
+                    <div className='front-info'>
+                        <p className='mission'>
+                            Our mission is to help promote sun awareness with a
+                            simple, fun and interactive system. We encourage our
+                            users to track the amount of sun exposure they are
+                            receiving on a day to day basis. We display our
+                            users their data to help them visually keep track of
+                            there exposure, along with providing resources to
+                            help care for there skin.
+                        </p>
+                        <div className='home-buttons'>
+                            <a
+                                className='btn btn-primary'
+                                id='left-button'
+                                href={sign_in_route}
+                            >
+                                Sign in
+                            </a>
+                            <Link
+                                to='/aboutus'
+                                className='btn btn-outline-primary'
+                                id='right-button'
+                            >
+                                About us
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-          </React.Fragment>
-    );
-  }
+            </React.Fragment>
+        )
+    }
 }
 
 export default Home
